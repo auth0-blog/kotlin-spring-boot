6dd7c47955ebea556372987d42bd5faaf6cf8f48
bruno.krebs <krebs.bruno@gmail.com>
2017-06-07 16:04:57 -0300

    Cleaning stuff up

--- src/main/kotlin/com/auth0/samples/kotlinspringboot/persistence/CustomerRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.auth0.samples.kotlinspringboot.persistence
-
-import com.auth0.samples.kotlinspringboot.model.Customer
-import org.springframework.data.repository.CrudRepository
-
-interface CustomerRepository : CrudRepository<Customer, Long> {
-	fun findByLastName(name: String): List<Customer>
-}
